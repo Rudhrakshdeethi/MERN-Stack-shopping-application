501c215bb022263e01cb7ae6ef9b41e7f6c6d370
Rudhrakshdeethi <rudhraksh319@gmail.com>
2026-05-06 12:11:15 +0530

    chore: configure Vercel deployment settings and export Express app for serverless compatibility

--- MERN-Stack-shopping-application-main/server.js
+++ MERN-Stack-shopping-application-main/server.js
@@ -14,15 +14,16 @@ app.use('/api/cart',require('./routes/api/cart'));
 app.use('/api/order',require('./routes/api/order'));
 app.use('/api/products',require('./routes/api/products'));
 
-// Serve static assets in production
+// Serve static assets in production (Handled by Vercel in this deployment)
+/*
 if (process.env.NODE_ENV === 'production') {
-  // Set static folder
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+*/
 
 
 const PORT = process.env.PORT || 5000 ;
